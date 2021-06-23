7b13ce85b4813547e0fe695090439f3addb194ef
Victor oc <ochando3d@gmail.com>
2021-06-23 17:05:48 +0100

    Basic players functionality, get all/by id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerDetailComponent } from './views/player-detail/player-detail.component';
+import { PlayersComponent } from './views/players/players.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'players',
+    pathMatch: 'full',
+  },
+  {
+    path: 'players',
+    component: PlayersComponent,
+  },
+  {
+    path: 'players/:id',
+    component: PlayerDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './views/players/players.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { PlayerDetailComponent } from './views/player-detail/player-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent,
+    NavbarComponent,
+    PlayerDetailComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
