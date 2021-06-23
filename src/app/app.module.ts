a2447a75f9862d1f1781fa3915a8c2b3a3610e24
Victor oc <ochando3d@gmail.com>
2021-06-23 19:28:34 +0100

    Players CRUD finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,7 @@ import { PlayerDetailComponent } from './views/player-detail/player-detail.compo
     NavbarComponent,
     PlayerDetailComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
